07842ef413b10ef79a49936b3575ad67287e2a48
XoDeR <xoder.vs@gmail.com>
2016-06-23 19:46:35 +0200

    Device/ Input Device
    Device/ Input Device

---
+++ Amstel/Src/Device/InputDevice.cpp
@@ -0,0 +1,183 @@
+// Copyright (c) 2016 Volodymyr Syvochka
+#include "Device/InputDevice.h"
+#include "Core/Error/Error.h"
+#include "Core/Memory/Allocator.h"
+#include "Core/Strings/StringUtils.h"
+#include "Core/Strings/StringId.h"
+
+#include <string.h> // strcpy, memset
+
+namespace Rio
+{
+
+const char* InputDevice::getName() const
+{
+	return inputDeviceName;
+}
+
+bool InputDevice::getIsConnected() const
+{
+	return isConnected;
+}
+
+uint8_t InputDevice::getButtonCount() const
+{
+	return buttonListCount;
+}
+
+uint8_t InputDevice::getAxesCount() const
+{
+	return axisListCount;
+}
+
+bool InputDevice::getIsPressed(uint8_t id) const
+{
+	RIO_ASSERT(id < buttonListCount, "Index out of bounds");
+	return (~lastStateList[id] & stateList[id]) != 0;
+}
+
+bool InputDevice::getIsReleased(uint8_t id) const
+{
+	RIO_ASSERT(id < buttonListCount, "Index out of bounds");
+	return (lastStateList[id] & ~stateList[id]) != 0;
+}
+
+bool InputDevice::getIsAnyPressed() const
+{
+	return getIsPressed(lastButton);
+}
+
+bool InputDevice::getIsAnyReleased() const
+{
+	return getIsReleased(lastButton);
+}
+
+Vector3 InputDevice::getAxis(uint8_t id) const
+{
+	RIO_ASSERT(id < axisListCount, "Index out of bounds");
+	return axisList[id];
+}
+
+const char* InputDevice::getButtonName(uint8_t id)
+{
+	RIO_ASSERT(id < buttonListCount, "Index out of bounds");
+	return buttonNameList[id];
+}
+
+const char* InputDevice::getAxisName(uint8_t id)
+{
+	RIO_ASSERT(id < axisListCount, "Index out of bounds");
+	return axisNameList[id];
+}
+
+uint8_t InputDevice::getButtonId(StringId32 name)
+{
+	for (uint32_t i = 0; i < buttonListCount; ++i)
+	{
+		if (buttonNameHashList[i] == name)
+		{
+			return i;
+		}
+	}
+
+	RIO_ASSERT(false, "Unknown button name");
+	return 0;
+}
+
+uint8_t InputDevice::getAxisId(StringId32 name)
+{
+	for (uint32_t i = 0; i < axisListCount; ++i)
+	{
+		if (axisNameHashList[i] == name)
+		{
+			return i;
+		}
+	}
+
+	RIO_ASSERT(false, "Unknown axis name");
+	return 0;
+}
+
+void InputDevice::setIsConnected(bool connected)
+{
+	this->isConnected = connected;
+}
+
+void InputDevice::setButtonState(uint8_t i, bool state)
+{
+	RIO_ASSERT(i < buttonListCount, "Index out of bounds");
+	lastButton = i;
+	stateList[i] = state;
+}
+
+void InputDevice::setAxis(uint8_t i, const Vector3& value)
+{
+	RIO_ASSERT(i < axisListCount, "Index out of bounds");
+	axisList[i] = value;
+}
+
+void InputDevice::update()
+{
+	memcpy(lastStateList, stateList, sizeof(uint8_t)*buttonListCount);
+}
+
+namespace InputDeviceFn
+{
+	InputDevice* create(Allocator& a, const char* name, uint8_t buttonListCount, uint8_t axesCount, const char** buttonNameList, const char** axisNameList)
+	{
+		const uint32_t size = 0
+			+ sizeof(InputDevice)
+			+ sizeof(uint8_t)*buttonListCount *2
+			+ sizeof(Vector3)*axesCount
+			+ sizeof(char*)*buttonListCount
+			+ sizeof(char*)*axesCount
+			+ sizeof(StringId32)*buttonListCount
+			+ sizeof(StringId32)*axesCount
+			+ getStringLength32(name) + 1
+			;
+
+		InputDevice* inputDevice = (InputDevice*)a.allocate(size);
+
+		inputDevice->isConnected = false;
+		inputDevice->buttonListCount = buttonListCount;
+		inputDevice->axisListCount = axesCount;
+		inputDevice->lastButton = 0;
+
+		inputDevice->lastStateList = (uint8_t*)&inputDevice[1];
+		inputDevice->stateList = (uint8_t*)(inputDevice->lastStateList + buttonListCount);
+		inputDevice->axisList = (Vector3*)(inputDevice->stateList + buttonListCount);
+		inputDevice->buttonNameList = (const char**)(inputDevice->axisList + axesCount);
+		inputDevice->axisNameList = (const char**)(inputDevice->buttonNameList + buttonListCount);
+		inputDevice->buttonNameHashList = (StringId32*)(inputDevice->axisNameList + axesCount);
+		inputDevice->axisNameHashList = (StringId32*)(inputDevice->buttonNameHashList + buttonListCount);
+		inputDevice->inputDeviceName = (char*)(inputDevice->axisNameHashList + axesCount);
+
+		memset(inputDevice->lastStateList, 0, sizeof(uint8_t)*buttonListCount);
+		memset(inputDevice->stateList, 0, sizeof(uint8_t)*buttonListCount);
+		memset(inputDevice->axisList, 0, sizeof(Vector3)*axesCount);
+		memcpy(inputDevice->buttonNameList, buttonNameList, sizeof(const char*)*buttonListCount);
+		memcpy(inputDevice->axisNameList, axisNameList, sizeof(const char*)*axesCount);
+
+		for (uint32_t i = 0; i < buttonListCount; ++i)
+		{
+			inputDevice->buttonNameHashList[i] = StringId32(buttonNameList[i]);
+		}
+
+		for (uint32_t i = 0; i < axesCount; ++i)
+		{
+			inputDevice->axisNameHashList[i] = StringId32(axisNameList[i]);
+		}
+
+		strcpy(inputDevice->inputDeviceName, name);
+
+		return inputDevice;
+	}
+
+	void destroy(Allocator& a, InputDevice& inputDevice)
+	{
+		a.deallocate(&inputDevice);
+	}
+} // namespace InputDeviceFn
+
+} // namespace Rio
+// Copyright (c) 2016 Volodymyr Syvochka
---
+++ Amstel/Src/Device/InputDevice.h
@@ -0,0 +1,67 @@
+// Copyright (c) 2016 Volodymyr Syvochka
+#pragma once
+
+#include "Core/Base/Types.h"
+#include "Core/Math/MathTypes.h"
+#include "Core/Memory/MemoryTypes.h"
+#include "Core/Strings/StringId.h"
+
+namespace Rio
+{
+
+struct InputDevice
+{
+	// Returns the name of the input device
+	const char* getName() const;
+	// Returns whether the input device is connected and functioning
+	bool getIsConnected() const;
+	// Returns the number of buttons of the input device
+	uint8_t getButtonCount() const;
+	// Returns the number of axes of the input devices
+	uint8_t getAxesCount() const;
+	// Returns whether the button <id> is pressed in the current frame
+	bool getIsPressed(uint8_t id) const;
+	// Returns whether the button <id> is released in the current frame
+	bool getIsReleased(uint8_t id) const;
+	// Returns whether any button is pressed in the current frame
+	bool getIsAnyPressed() const;
+	// Returns whether any button is released in the current frame
+	bool getIsAnyReleased() const;
+	// Returns the value of the axis <id>
+	Vector3 getAxis(uint8_t id) const;
+	// Returns the name of the button <id>
+	const char* getButtonName(uint8_t id);
+	// Returns the name of the axis <id>
+	const char* getAxisName(uint8_t id);
+	// Returns the id of the button <name>
+	uint8_t getButtonId(StringId32 name);
+	// Returns the id of the axis <name>
+	uint8_t getAxisId(StringId32 name);
+	void setIsConnected(bool connected);
+	void setButtonState(uint8_t i, bool state);
+	void setAxis(uint8_t i, const Vector3& value);
+	void update();
+
+	bool isConnected = false;
+	uint8_t buttonListCount;
+	uint8_t axisListCount;
+	uint8_t lastButton;
+
+	uint8_t* lastStateList; // buttonListCount
+	uint8_t* stateList;  // buttonListCount
+	Vector3* axisList; // axesCount
+	const char** buttonNameList; // buttonListCount
+	const char** axisNameList;   // axesCount
+	StringId32* buttonNameHashList;  // buttonListCount
+	StringId32* axisNameHashList;    // axesCount
+	char* inputDeviceName; // getStringLength32(name) + 1
+};
+
+namespace InputDeviceFn
+{
+	InputDevice* create(Allocator& a, const char* name, uint8_t buttonListCount, uint8_t axesCount, const char** buttonNameList, const char** axisNameList);
+	void destroy(Allocator& a, InputDevice& id);
+} // namespace InputDeviceFn
+
+} // namespace Rio
+// Copyright (c) 2016 Volodymyr Syvochka
