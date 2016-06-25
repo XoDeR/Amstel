930131d2f36c2300f93983f8debe093a7bd46b27
XoDeR <xoder.vs@gmail.com>
2016-06-25 20:31:55 +0200

    Resource/ Unit Resource
    Resource/ Unit Resource

---
+++ Amstel/Src/Resource/UnitResource.cpp
@@ -0,0 +1,41 @@
+// Copyright (c) 2016 Volodymyr Syvochka
+#include "Resource/UnitResource.h"
+#include "Core/Memory/Allocator.h"
+#include "Core/FileSystem/File.h"
+#include "Core/FileSystem/FileSystem.h"
+#include "Resource/ResourceTypes.h"
+#include "Resource/UnitCompiler.h"
+
+namespace Rio
+{
+
+namespace UnitResourceFn
+{
+	void compile(const char* path, CompileOptions& compileOptions)
+	{
+		Buffer unitData(getDefaultAllocator());
+
+		UnitCompiler unitCompiler(compileOptions);
+		unitCompiler.compileUnit(path);
+
+		compileOptions.write(unitCompiler.getBlob());
+	}
+
+	void* load(File& file, Allocator& a)
+	{
+		const uint32_t size = file.getSize();
+		void* resource = a.allocate(size);
+		file.read(resource, size);
+		RIO_ASSERT(*(uint32_t*)resource == RESOURCE_VERSION_UNIT, "Wrong version");
+		return resource;
+	}
+
+	void unload(Allocator& a, void* resource)
+	{
+		a.deallocate(resource);
+	}
+
+} // namespace UnitResourceFn
+
+} // namespace Rio
+// Copyright (c) 2016 Volodymyr Syvochka
---
+++ Amstel/Src/Resource/UnitResource.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2016 Volodymyr Syvochka
+#pragma once
+
+#include "Core/Memory/MemoryTypes.h"
+#include "Core/FileSystem/FileSystemTypes.h"
+#include "Resource/CompilerTypes.h"
+
+namespace Rio
+{
+
+struct UnitResource
+{
+	uint32_t version;
+	uint32_t unitListCount;
+	uint32_t componentTypesCount;
+//	ComponentData data[componentTypesCount]
+};
+
+struct ComponentData
+{
+	StringId32 type;
+	// entity
+	uint32_t instancesCount;
+	uint32_t size;
+//	uint32_t unitIndexList[instancesCount]
+//	char data[size]
+};
+
+namespace UnitResourceFn
+{
+	void compile(const char* path, CompileOptions& compileOptions);
+	void* load(File& file, Allocator& a);
+	void unload(Allocator& allocator, void* resource);
+} // namespace UnitResourceFn
+
+} // namespace Rio
+// Copyright (c) 2016 Volodymyr Syvochka
