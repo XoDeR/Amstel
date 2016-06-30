a1a73647f85813c6aa252cdc6a1fbb36909752d1
XoDeR <xoder.vs@gmail.com>
2016-06-30 18:16:19 +0200

    World/ Audio; Physics
    World/ Audio; Physics

---
+++ Amstel/Src/World/Audio.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2016 Volodymyr Syvochka
+#pragma once
+
+namespace Rio
+{
+
+namespace AudioGlobalFn
+{
+	void init();
+	void shutdown();
+} // namespace AudioGlobalFn
+
+} // namespace Rio
+// Copyright (c) 2016 Volodymyr Syvochka
---
+++ Amstel/Src/World/Physics.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2016 Volodymyr Syvochka
+#pragma once
+
+#include "Core/Memory/MemoryTypes.h"
+
+namespace Rio
+{
+
+namespace PhysicsGlobalFn
+{
+	void init(Allocator& a);
+	void shutdown(Allocator& a);
+} // namespace PhysicsGlobalFn
+
+} // namespace Rio
+// Copyright (c) 2016 Volodymyr Syvochka
