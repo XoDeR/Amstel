fd91942ea5608811a050f8cf05e8ae8cfe9465b9
XoDeR <xoder.vs@gmail.com>
2016-07-02 21:47:19 +0200

    World/ Sound World
    World/ Sound World

---
+++ Amstel/Src/World/SoundWorld.h
@@ -0,0 +1,50 @@
+// Copyright (c) 2016 Volodymyr Syvochka
+#pragma once
+
+#include "Core/Math/MathTypes.h"
+#include "Core/Memory/MemoryTypes.h"
+#include "Core/Base/Types.h"
+#include "Resource/ResourceTypes.h"
+#include "World/WorldTypes.h"
+
+namespace Rio
+{
+
+class SoundWorld
+{
+public:
+	virtual ~SoundWorld() {};
+	// Plays the sound <soundResource> at the given <volume> [0 .. 1]
+	// If loop is true the sound will be played looping
+	virtual SoundInstanceId play(const SoundResource& soundResource, bool loop, float volume, float range, const Vector3& position) = 0;
+	// Stops the sound with the given <id>
+	// After this call, the instance will be destroyed
+	virtual void stop(SoundInstanceId id) = 0;
+	// Returns whether the sound <id> is playing
+	virtual bool getIsPlaying(SoundInstanceId id) = 0;
+	// Stops all the sounds in the world
+	virtual void stopAll() = 0;
+	// Pauses all the sounds in the world
+	virtual void pauseAll() = 0;
+	// Resumes all previously paused sounds in the world
+	virtual void resumeAll() = 0;
+	// Sets the <positions> (in world space) of <soundInstanceListCount> sound instances <soundInstanceIdList>
+	virtual void setSoundPositions(uint32_t soundInstanceListCount, const SoundInstanceId* soundInstanceIdList, const Vector3* positions) = 0;
+	// Sets the <ranges> (in meters) of <soundInstanceListCount> sound instances <soundInstanceIdList>
+	virtual void setSoundRanges(uint32_t soundInstanceListCount, const SoundInstanceId* soundInstanceIdList, const float* ranges) = 0;
+	// Sets the <volumes> of <soundInstanceListCount> sound instances <soundInstanceIdList>
+	virtual void setSoundVolumes(uint32_t soundInstanceListCount, const SoundInstanceId* soundInstanceIdList, const float* volumes) = 0;
+	virtual void reloadSounds(const SoundResource& oldSoundResource, const SoundResource& newSoundResource) = 0;
+	// Sets the <pose> of the listener in world space
+	virtual void setListenerPose(const Matrix4x4& pose) = 0;
+	virtual void update() = 0;
+};
+
+namespace SoundWorldFn
+{
+	SoundWorld* create(Allocator& a);
+	void destroy(Allocator& a, SoundWorld* soundWorld);
+} // namespace SoundWorldFn
+
+} // namespace Rio
+// Copyright (c) 2016 Volodymyr Syvochka
---
+++ Amstel/Src/World/SoundWorldNull.cpp
@@ -0,0 +1,100 @@
+// Copyright (c) 2016 Volodymyr Syvochka
+#include "Config.h"
+
+#if RIO_SOUND_NULL
+
+#include "Core/Memory/Memory.h"
+#include "World/SoundWorld.h"
+#include "World/Audio.h"
+
+namespace Rio
+{
+
+namespace AudioGlobalFn
+{
+	void init()
+	{
+	}
+
+	void shutdown()
+	{
+	}
+}
+
+class NullSoundWorld : public SoundWorld
+{
+public:
+
+	NullSoundWorld()
+	{
+	}
+
+	virtual ~NullSoundWorld()
+	{
+	}
+
+	virtual SoundInstanceId play(const SoundResource& /*soundResource*/, bool /*loop*/, float /*volume*/, float /*range*/, const Vector3& /*position*/)
+	{
+		return 0;
+	}
+
+	virtual void stop(SoundInstanceId /*id*/)
+	{
+	}
+
+	virtual bool getIsPlaying(SoundInstanceId /*id*/)
+	{
+		return false;
+	}
+
+	virtual void stopAll()
+	{
+	}
+
+	virtual void pauseAll()
+	{
+	}
+
+	virtual void resumeAll()
+	{
+	}
+
+	virtual void setSoundPositions(uint32_t /*soundInstanceListCount*/, const SoundInstanceId* /*soundInstanceIdList*/, const Vector3* /*positions*/)
+	{
+	}
+
+	virtual void setSoundRanges(uint32_t /*soundInstanceListCount*/, const SoundInstanceId* /*soundInstanceIdList*/, const float* /*ranges*/)
+	{
+	}
+
+	virtual void setSoundVolumes(uint32_t /*soundInstanceListCount*/, const SoundInstanceId* /*soundInstanceIdList*/, const float* /*volumes*/)
+	{
+	}
+
+	virtual void reloadSounds(const SoundResource& /*oldSoundResource*/, const SoundResource& /*newSoundResource*/)
+	{
+	}
+
+	virtual void setListenerPose(const Matrix4x4& /*pose*/)
+	{
+	}
+
+	virtual void update()
+	{
+	}
+};
+
+SoundWorld* SoundWorldFn::create(Allocator& a)
+{
+	return RIO_NEW(a, NullSoundWorld)();
+}
+
+void SoundWorldFn::destroy(Allocator& a, SoundWorld* soundWorld)
+{
+	RIO_DELETE(a, soundWorld);
+}
+
+} // namespace Rio
+
+#endif // RIO_SOUND_NULL
+// Copyright (c) 2016 Volodymyr Syvochka
