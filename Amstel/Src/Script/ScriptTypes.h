35beb8a858d2eb2ed6a309c55410ab70b13e2e12
XoDeR <xoder.vs@gmail.com>
2016-07-04 19:16:36 +0200

    Script/ Script Types
    Script/ Script Types

---
+++ Amstel/Src/Script/ScriptTypes.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2016 Volodymyr Syvochka
+#pragma once
+
+namespace Rio
+{
+
+struct ScriptStack;
+struct ScriptEnvironment;
+
+} // namespace Rio
+// Copyright (c) 2016 Volodymyr Syvochka
